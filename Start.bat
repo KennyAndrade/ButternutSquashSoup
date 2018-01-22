a8a1c6198ef9232779849c9385e0a8cccbd75d74
EMERGE-VPS <mikemaddem@users.noreply.github.com>
2018-01-21 18:05:51 -0800

    Server Fixes

---
+++ Start.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+
+TIMEOUT /T 1
+cd C:/users/administrator/desktop/FIVEM Server/ButternutSquashSoup
+TIMEOUT /T 1
+run.cmd +exec server.cfg
+pause >nul
--- resources/vrp/cfg/base.lua
+++ resources/vrp/cfg/base.lua
@@ -4,7 +4,7 @@ local cfg = {}
 -- mysql credentials
 cfg.db = {
   host = "127.0.0.1", -- database ip (default is local)
-  database = "gtavmm52103",   -- name of database
+  database = "fivem",   -- name of database
   user = "root",    --  database username
   password = ""   -- password of your database
 }
