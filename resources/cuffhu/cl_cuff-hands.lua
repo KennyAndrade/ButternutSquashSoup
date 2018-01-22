b22c083c19beb7e091d23501e33e30dd0aeeef71
Kenny <spikelcdoj@gmail.com>
2018-01-21 19:09:33 -0800

    New Cuff Script | New GUI

---
+++ resources/cuffhu/cl_cuff-hands.lua
@@ -0,0 +1,119 @@
+local handcuff = false
+
+--[[ HANDCUFF SCRIPT ]]--
+function loadAnimDict( dict )
+    while ( not HasAnimDictLoaded( dict ) ) do
+        RequestAnimDict( dict )
+        Citizen.Wait( 5 )
+    end
+end
+
+RegisterNetEvent( 'KneelHU' )
+AddEventHandler( 'KneelHU', function()
+	local ped = GetPlayerPed( -1 )
+	local wep = GetSelectedPedWeapon(ped)
+    if ( DoesEntityExist( ped ) and not IsEntityDead( ped )) then
+        loadAnimDict( "random@arrests" )
+        loadAnimDict( "random@arrests@busted" )
+        if ( IsEntityPlayingAnim( ped, "random@arrests@busted", "idle_a", 3 ) ) then
+	    TaskPlayAnim( ped, "random@arrests@busted", "exit", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (3000)
+            TaskPlayAnim( ped, "random@arrests", "kneeling_arrest_get_up", 8.0, 1.0, -1, 128, 0, 0, 0, 0 )
+	    SetEnableHandcuffs(ped, false)
+	    SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
+        else
+            ClearPedSecondaryTask(ped)
+	    SetEnableHandcuffs(ped, true)
+	    TaskPlayAnim( ped, "random@arrests", "idle_2_hands_up", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (4000)
+	    SetPedDropsInventoryWeapon(ped, wep, 0, 2.0, 0, -1)
+	    SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
+            TaskPlayAnim( ped, "random@arrests", "kneeling_arrest_idle", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (500)
+            TaskPlayAnim( ped, "random@arrests@busted", "enter", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (1000)
+            TaskPlayAnim( ped, "random@arrests@busted", "idle_a", 8.0, 1.0, -1, 9, 0, 0, 0, 0 )
+        end    
+    end
+end )
+ 
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests@busted", "idle_a", 3) then
+            DisableControlAction(0, 21, true)
+	    DisableControlAction(1, 140, true)
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        end
+    end
+end)
+--]]
+
+--[[---------------------]]--
+
+--[[ HANDSUP SCRIPT ]]--
+RegisterNetEvent("Handsup")
+AddEventHandler("Handsup", function()
+	local lPed = GetPlayerPed(-1)
+	if DoesEntityExist(lPed) then
+		if not IsEntityPlayingAnim(lPed, "mp_arresting", "idle", 3) then
+			RequestAnimDict("random@mugging3")
+			while not HasAnimDictLoaded("random@mugging3") do
+				Citizen.Wait(100)
+			end
+			
+			if IsEntityPlayingAnim(lPed, "random@mugging3", "handsup_standing_base", 3) then
+				ClearPedSecondaryTask(lPed)
+				SetEnableHandcuffs(lPed, false)
+				SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
+				TriggerEvent("chatMessage", "", {255, 0, 0}, "Your have put your hands down.")
+			else
+				TaskPlayAnim(lPed, "random@mugging3", "handsup_standing_base", 8.0, -8, -1, 49, 0, 0, 0, 0)
+				SetEnableHandcuffs(lPed, true)
+				SetCurrentPedWeapon(lPed, GetHashKey("WEAPON_UNARMED"), true)
+				TriggerEvent("chatMessage", "", {255, 0, 0}, "Your hands are up.")
+			end
+		else
+			TriggerEvent("chatMessage", "You are handcuffed..")
+		end
+	end
+end)
+--]]
+
+--[[---------------------]]--
+
+--[[ Other Functions ]]--
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if handcuff and not IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "mp_arresting", "idle", 3) then
+			Citizen.Wait(3000)
+			Citizen.Trace("BACKUP CUFFING TRIGGERED")
+			TaskPlayAnim(GetPlayerPed(PlayerId()), "mp_arresting", "idle", 8.0, -8, -1, 49, 0, 0, 0, 0)
+		end
+
+		if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "mp_arresting", "idle", 3) then
+			DisableControlAction(1, 140, true)
+			DisableControlAction(1, 141, true)
+			DisableControlAction(1, 142, true)
+			SetPedPathCanUseLadders(GetPlayerPed(PlayerId()), false)
+			if IsPedInAnyVehicle(GetPlayerPed(PlayerId()), false) then
+				DisableControlAction(0, 59, true)
+			end
+		end
+
+		if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@mugging3", "handsup_standing_base", 3) then
+			DisableControlAction(1, 140, true)
+			DisableControlAction(1, 141, true)
+			DisableControlAction(1, 142, true)
+			if IsPedInAnyVehicle(GetPlayerPed(PlayerId()), false) then
+				DisableControlAction(0, 59, true)
+			end
+		end
+	end
+end)
+
+--]]
