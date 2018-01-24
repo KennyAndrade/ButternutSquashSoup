21b8646782142c012703c5a7eaf91d9874abd9ab
Kenny <spikelcdoj@gmail.com>
2018-01-23 22:01:39 -0800

    pls let this work i dont want a shitty gui menu

--- resources/extras/extras_client.lua
+++ resources/extras/extras_client.lua
@@ -2,62 +2,74 @@ local Veh = GetVehiclePedIsUsing(GetPlayerPed(-1))
 
 RegisterNetEvent("e1")
 AddEventHandler("e1", function()
-SetVehicleExtra(veh, 1, true)
+if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 1, true)
 		end
 	end
-	RegisterNetEvent("e2")
+RegisterNetEvent("e2")
 AddEventHandler("e2", function()
-SetVehicleExtra(veh, 2, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 2, true)
 		end
 	end
-	RegisterNetEvent("e3")
+RegisterNetEvent("e3")
 AddEventHandler("e3", function()
-SetVehicleExtra(veh, 3, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 3, true)
 		end
 	end
-	RegisterNetEvent("e4")
+RegisterNetEvent("e4")
 AddEventHandler("e4", function()
-SetVehicleExtra(veh, 4, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 4, true)
 		end
 	end
-	RegisterNetEvent("e5")
+RegisterNetEvent("e5")
 AddEventHandler("e5", function()
-SetVehicleExtra(veh, 5, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 5, true)
 		end
 	end
-	RegisterNetEvent("e6")
+RegisterNetEvent("e6")
 AddEventHandler("e6", function()
-SetVehicleExtra(veh, 6, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 6, true)
 		end
 	end
-	RegisterNetEvent("e7")
+RegisterNetEvent("e7")
 AddEventHandler("e7", function()
-SetVehicleExtra(veh, 7, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 7, true)
 		end
 	end
-	RegisterNetEvent("e8")
+RegisterNetEvent("e8")
 AddEventHandler("e8", function()
-SetVehicleExtra(veh, 8, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 8, true)
 		end
 	end
-	RegisterNetEvent("e9")
+RegisterNetEvent("e9")
 AddEventHandler("e9", function()
-SetVehicleExtra(veh, 9, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 9, true)
 		end
 	end
-	RegisterNetEvent("e1true")
+RegisterNetEvent("e10")
 AddEventHandler("e1true", function()
-SetVehicleExtra(veh, 1true, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 1true, true)
 		end
 	end
-	RegisterNetEvent("e11")
+RegisterNetEvent("e11")
 AddEventHandler("e11", function()
-SetVehicleExtra(veh, 11, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 11, true)
 		end
 	end
-	RegisterNetEvent("e12")
+RegisterNetEvent("e12")
 AddEventHandler("e12", function()
-SetVehicleExtra(veh, 12, true)
+	if DoesEntityExist(Veh) then
+SetVehicleExtra(Veh, 12, true)
 		end
 	end
 end)
