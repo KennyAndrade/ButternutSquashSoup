48ac4610e15ac1df75a4b17dfe96637c9a335cd3
Kenny <spikelcdoj@gmail.com>
2018-01-23 21:55:41 -0800

    TIL Lua is case sensitive

--- resources/extras/extras_client.lua
+++ resources/extras/extras_client.lua
@@ -2,62 +2,62 @@ local Veh = GetVehiclePedIsUsing(GetPlayerPed(-1))
 
 RegisterNetEvent("e1")
 AddEventHandler("e1", function()
-SetVehicleExtra(veh, 1, 0)
+SetVehicleExtra(veh, 1, true)
 		end
 	end
 	RegisterNetEvent("e2")
 AddEventHandler("e2", function()
-SetVehicleExtra(veh, 2, 0)
+SetVehicleExtra(veh, 2, true)
 		end
 	end
 	RegisterNetEvent("e3")
 AddEventHandler("e3", function()
-SetVehicleExtra(veh, 3, 0)
+SetVehicleExtra(veh, 3, true)
 		end
 	end
 	RegisterNetEvent("e4")
 AddEventHandler("e4", function()
-SetVehicleExtra(veh, 4, 0)
+SetVehicleExtra(veh, 4, true)
 		end
 	end
 	RegisterNetEvent("e5")
 AddEventHandler("e5", function()
-SetVehicleExtra(veh, 5, 0)
+SetVehicleExtra(veh, 5, true)
 		end
 	end
 	RegisterNetEvent("e6")
 AddEventHandler("e6", function()
-SetVehicleExtra(veh, 6, 0)
+SetVehicleExtra(veh, 6, true)
 		end
 	end
 	RegisterNetEvent("e7")
 AddEventHandler("e7", function()
-SetVehicleExtra(veh, 7, 0)
+SetVehicleExtra(veh, 7, true)
 		end
 	end
 	RegisterNetEvent("e8")
 AddEventHandler("e8", function()
-SetVehicleExtra(veh, 8, 0)
+SetVehicleExtra(veh, 8, true)
 		end
 	end
 	RegisterNetEvent("e9")
 AddEventHandler("e9", function()
-SetVehicleExtra(veh, 9, 0)
+SetVehicleExtra(veh, 9, true)
 		end
 	end
-	RegisterNetEvent("e10")
-AddEventHandler("e10", function()
-SetVehicleExtra(veh, 10, 0)
+	RegisterNetEvent("e1true")
+AddEventHandler("e1true", function()
+SetVehicleExtra(veh, 1true, true)
 		end
 	end
 	RegisterNetEvent("e11")
 AddEventHandler("e11", function()
-SetVehicleExtra(veh, 11, 0)
+SetVehicleExtra(veh, 11, true)
 		end
 	end
 	RegisterNetEvent("e12")
 AddEventHandler("e12", function()
-SetVehicleExtra(veh, 12, 0)
+SetVehicleExtra(veh, 12, true)
 		end
 	end
 end)
