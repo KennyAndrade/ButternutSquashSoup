59ecb95c1f545db1997afb38e45d5a21b7fe4fc6
Kenny <spikelcdoj@gmail.com>
2018-01-23 22:05:41 -0800

    abort me

--- resources/extras/extras_client.lua
+++ resources/extras/extras_client.lua
@@ -5,71 +5,70 @@ AddEventHandler("e1", function()
 if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 1, true)
 		end
-	end
+	end)
 RegisterNetEvent("e2")
 AddEventHandler("e2", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 2, true)
 		end
-	end
+	end)
 RegisterNetEvent("e3")
 AddEventHandler("e3", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 3, true)
 		end
-	end
+	end)
 RegisterNetEvent("e4")
 AddEventHandler("e4", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 4, true)
 		end
-	end
+	end)
 RegisterNetEvent("e5")
 AddEventHandler("e5", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 5, true)
 		end
-	end
+	end)
 RegisterNetEvent("e6")
 AddEventHandler("e6", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 6, true)
 		end
-	end
+	end)
 RegisterNetEvent("e7")
 AddEventHandler("e7", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 7, true)
 		end
-	end
+	end)
 RegisterNetEvent("e8")
 AddEventHandler("e8", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 8, true)
 		end
-	end
+	end)
 RegisterNetEvent("e9")
 AddEventHandler("e9", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 9, true)
 		end
-	end
+	end)
 RegisterNetEvent("e10")
 AddEventHandler("e1true", function()
 	if DoesEntityExist(Veh) then
-SetVehicleExtra(Veh, 1true, true)
+SetVehicleExtra(Veh, 10, true)
 		end
-	end
+	end)
 RegisterNetEvent("e11")
 AddEventHandler("e11", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 11, true)
 		end
-	end
+	end)
 RegisterNetEvent("e12")
 AddEventHandler("e12", function()
 	if DoesEntityExist(Veh) then
 SetVehicleExtra(Veh, 12, true)
-		end
-	end
-end)
+      end
+    end)
