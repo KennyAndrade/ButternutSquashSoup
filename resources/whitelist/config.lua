1b179c8466fc26682795d8e0e75f3b8901fefcbc
Mike M <mikemaddem@users.noreply.github.com>
2018-01-27 20:00:43 -0800

    PLD / Speedometer

---
+++ resources/whitelist/config.lua
@@ -0,0 +1,14 @@
+mode = 'whitelist'
+
+-- Remember, the last ID must have NO COMMA at the end
+
+steamIdsList = {
+  '76561198056313973', -- Dunko's
+  '76561198356265492',
+  '76561197997106251',
+  '76561198077331588',
+  '76561198055963494',
+  '76561198295531167'  -- jake (civ) testing
+  }
+
+errorMessage = 'This is a whitelisted server.'
