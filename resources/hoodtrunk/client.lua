97ce81f38056035a91af6d5f15c9b1b5a2fc49f7
Kenny <spikelcdoj@gmail.com>
2018-02-05 20:28:13 -0800

    ecksdee4

--- resources/hoodtrunk/client.lua
+++ resources/hoodtrunk/client.lua
@@ -21,3 +21,27 @@ RegisterCommand("trunk", function()
         end
     end
 end, false)
+
+RegisterCommand("bleft", function()
+    local ped = PlayerPedId()
+    local veh = GetVehiclePedIsIn(ped, false)
+    if veh ~= nil and veh ~= 0 and veh ~= 1 then
+        if GetVehicleDoorAngleRatio(veh, 2) > 0 then
+            SetVehicleDoorShut(veh, 2, false)
+        else
+            SetVehicleDoorOpen(veh, 2, false, false)
+        end
+    end
+end, false)
+
+RegisterCommand("bright", function()
+    local ped = PlayerPedId()
+    local veh = GetVehiclePedIsIn(ped, false)
+    if veh ~= nil and veh ~= 0 and veh ~= 1 then
+        if GetVehicleDoorAngleRatio(veh, 3) > 0 then
+            SetVehicleDoorShut(veh, 3, false)
+        else
+            SetVehicleDoorOpen(veh, 3, false, false)
+        end
+    end
+end, false)
