8a25fb0bf665ec3fc0d40a8bf881e9a225c102e2
Kenny <spikelcdoj@gmail.com>
2018-02-12 23:10:30 -0800

    ss

--- resources/cancelchat/server.lua
+++ resources/cancelchat/server.lua
@@ -11,10 +11,6 @@ AddEventHandler('chatMessage', function(source, name, message)
       TriggerClientEvent('chatMessage', -1, 'Cancel Chat', {255, 255, 255}, name .. ' has enabled chat for everyone.')
     end
     CancelEvent()
-  else
-    if cchat then
-      TriggerClientEvent('chatMessage', source, 'Cancel Chat', {255, 255, 255}, name .. ' has canceled chat for everyone, your message was deleted.')
-      CancelEvent()
     end
   end
 end)
