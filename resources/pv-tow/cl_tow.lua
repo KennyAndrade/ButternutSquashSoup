e5672c58021498640e647c5b437b84ee4cd97249
Kenny <spikelcdoj@gmail.com>
2018-02-12 17:06:57 -0800

    ds

--- resources/pv-tow/cl_tow.lua
+++ resources/pv-tow/cl_tow.lua
@@ -7,9 +7,40 @@ AddEventHandler('pv:tow', function()
 	local vehicle = GetVehiclePedIsIn(playerped, true)
 	
 	local towmodel = GetHashKey('flatbed')
+	local towmodel2 = GetHashKey('flatbed2')
 	local isVehicleTow = IsVehicleModel(vehicle, towmodel)
+	local isVehicleTow2 = IsVehicleModel(vehicle, towmodel2)
 			
 	if isVehicleTow then
+	
+		local coordA = GetEntityCoords(playerped, 1)
+		local coordB = GetOffsetFromEntityInWorldCoords(playerped, 0.0, 5.0, 0.0)
+		local targetVehicle = getVehicleInDirection(coordA, coordB)
+		
+		if currentlyTowedVehicle == nil then
+			if targetVehicle ~= 0 then
+				if not IsPedInAnyVehicle(playerped, true) then
+					if vehicle ~= targetVehicle then
+						AttachEntityToEntity(targetVehicle, vehicle, 20, -0.5, -5.0, 1.0, 0.0, 0.0, 0.0, false, false, false, false, 20, true)
+						currentlyTowedVehicle = targetVehicle
+						TriggerEvent("chatMessage", "[TOWFuckers]", {255, 255, 0}, "Vehicle successfully attached to towtruck!")
+					else
+						TriggerEvent("chatMessage", "[TOWFuckers]", {255, 255, 0}, "Are you retarded? You cant tow your own towtruck with your own towtruck?")
+					end
+				end
+			else
+				TriggerEvent("chatMessage", "[TOWFuckers]", {255, 255, 0}, "Theres no vehicle to tow?")
+			end
+		else
+			AttachEntityToEntity(currentlyTowedVehicle, vehicle, 20, -0.5, -12.0, 1.0, 0.0, 0.0, 0.0, false, false, false, false, 20, true)
+			DetachEntity(currentlyTowedVehicle, true, true)
+			currentlyTowedVehicle = nil
+			TriggerEvent("chatMessage", "[TOWFuckers]", {255, 255, 0}, "The vehicle has been successfully detached!")
+		end
+	end
+end)
+
+	if isVehicleTow2 then
 	
 		local coordA = GetEntityCoords(playerped, 1)
 		local coordB = GetOffsetFromEntityInWorldCoords(playerped, 0.0, 5.0, 0.0)
