90bd765ae54364b2e3125c2d25ab252daa8c0d92
Kenny <spikelcdoj@gmail.com>
2018-02-23 15:41:53 -0800

    xx

--- resources/vrp_hotkeys/cfg/hotkeys.lua
+++ resources/vrp_hotkeys/cfg/hotkeys.lua
@@ -1,48 +1,45 @@
--- TUNNEL AND PROXY
 cfg = {}
 vRPhk = {}
-Tunnel.bindInterface("vrp_hotkeys",vRPhk)
-vRPserver = Tunnel.getInterface("vRP","vrp_hotkeys")
-HKserver = Tunnel.getInterface("vrp_hotkeys","vrp_hotkeys")
+Tunnel.bindInterface("vrp_hotkeys", vRPhk)
+vRPserver = Tunnel.getInterface("vRP", "vrp_hotkeys")
+HKserver = Tunnel.getInterface("vrp_hotkeys", "vrp_hotkeys")
 vRP = Proxy.getInterface("vRP")
-
 -- GLOBAL VARIABLES
 called = 0
 
 -- YOU ARE ON A CLIENT SCRIPT ( Just reminding you ;) )
 -- Keys IDs can be found at https://wiki.fivem.net/wiki/Controls
-
 -- Hotkeys Configuration: cfg.hotkeys = {[Key] = {group = 1, pressed = function() end, released = function() end},}
 cfg.hotkeys = {
-  [46] = {
-    -- E call/skip emergency
-    group = 0, 
-	pressed = function() 
-	  if vRP.isInComa({}) then
-	    if called == 0 then
-	      HKserver.canSkipComa({},function(skipper)
-		    if skipper then
-		      HKserver.docsOnline({},function(docs)
-		        if docs == 0 then
-				  vRP.killComa({})
-			    else
-				  called = 30
-				  local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1),true))
-				  HKserver.helpComa({x,y,z})
-			    end
-			  end)
+    [46] = {
+        -- E call/skip emergency
+        group = 0,
+        pressed = function()
+            if vRP.isInComa({}) then
+                if called == 0 then
+                    HKserver.canSkipComa({}, function(skipper)
+                        if skipper then
+                            HKserver.docsOnline({}, function(docs)
+                                if docs == 0 then
+                                    vRP.killComa({})
+                                else
+                                    called = 30
+                                    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+                                    HKserver.helpComa({x, y, z})
+                                end
+                            end)
+                        end
+                    end)
+                else
+                    vRP.notify({"~r~You already called the ambulance."})
+                end
             end
-		  end)
-		else
-		  vRP.notify({"~r~You already called the ambulance."})
-		end
-	  end
-	end,
-	released = function()
-	  -- Do nothing on release because it's toggle.
-	end,
-  },
-    [213] = {
+        end,
+        released = function() end
+    }
+}
+-- Do nothing on release because it's toggle.
+--[[[213] = {
     --home on numberpad toggle User List
     group = 0, 
 	pressed = function() 
@@ -51,5 +48,4 @@ cfg.hotkeys = {
 	released = function()
 	  -- Do nothing on release because it's toggle.
 	end,
-  },
-}
+  },--]]
